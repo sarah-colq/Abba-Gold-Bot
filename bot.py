47d09ee34aeb3afb184338cdb7f30b5d03284ec6
SarahC <38871276+sarah-colq@users.noreply.github.com>
2019-04-24 18:10:18 -0400

    lyrics partially done

--- bot.py
+++ bot.py
@@ -1,20 +1,31 @@
 from keys import consumer_key, consumer_secret, access_token, access_token_secret
 import tweepy
-import time
+
 
 def get_lyrics():
-    lyrics = ''
+    lyrics = 'Waterloo I was defeated, you won the war'
     return lyrics
 
 def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
-    time_interval = 60*60*2
-    while True:
-        lyrics = get_lyrics()
-        api.update_status(lyrics)
-        time.sleep(time_interval)
+
+    search = 'abba'
+
+    search = "Keyword"
+    numberOfTweets = 20
+    for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+        try:
+            if tweet.user.name != 'abbashitpost':
+                tweet.favorite()
+        except tweepy.TweepError as e:
+            continue
+        except StopIteration:
+            break
+
+    lyrics = get_lyrics()
+    api.update_status(lyrics)
 
 if __name__== "__main__":
     main()
---
+++ lyrics.py
@@ -0,0 +1,39 @@
+lyrics_list =  ['You are the dancing queen\nYoung and sweet\nOnly seventeen',
+                'Dancing queen\nFeel the beat from the tambourine, oh yeah',
+                'You can dance\nYou can jive','Having the time of your life',
+                'Ooh, see that girl\nWatch that scene','Dig in the dancing queen',
+                'Knowing me, knowing you (a-ha)\nThere is nothing we can do'
+                'Knowing me, knowing you (a-ha)','We just have to face it this time we\'re through'
+                'Breaking up is never easy, I know\ nBut I have to go','Knowing me, knowing you',
+                'If you change your mind, I\'m the first in line','Honey I\'m still free\nTake a chance on me',
+                'If you need me, let me know, gonna be around','If you\'ve got no place to go, if you\'re feeling down',
+                'If you\'re all alone when the pretty birds have flown','Honey I\'m still free\nTake a chance on me',
+                'Gonna do my very best and it ain\'t no lie','If you put me to the test, if you let me try',
+                'Take a chance on me\n(Come on, give me a break will you?)','Take a chance on me',
+                'Mamma mia, here I go again','My my, how can I resist you?','Mamma mia, does it show again',
+                'My my, just how much I\'ve missed you?','Yes, I\'ve been brokenhearted\nBlue since the day we parted',
+                'Why, why did I ever let you go?','Mamma mia, even if I say\n\"Bye bye, leave me now or never\"',
+                'Mamma mia, it\'s a game we play\n\"Bye bye\" doesn\'t mean forever','Don\'t go wasting your emotion',
+                'Lay all your love on me','Don\'t go sharing your devotion','Lay all your love on me',
+                'Tonight the super trouper lights are gonna find me','Shining like the sun (sup-p-per troup-p-per)',
+                'Smiling, having fun (sup-p-per troup-p-per)','Feeling like a number one',
+                'Tonight the super trouper beams are gonna blind me','But I won\'t feel blue (sup-p-per troup-p-per)',
+                'Like I always do (sup-p-per troup-p-per)','Cause somewhere in the crowd there\'s you',
+                'I have a dream, a song to sing\nTo help me cope with anything','If you see the wonder of a fairy tale',
+                'You can take the future even if you fail','I believe in angels\nSomething good in everything I see',
+                'I believe in angels\nWhen I know the time is right for me','I\'ll cross the stream, I have a dream',
+                'I don\'t want to talk\nAbout the things we\'ve gone through','Though it\'s hurting me\nNow it\'s history',
+                'I\'ve played all my cards\nAnd that\'s what you\'ve done too','Nothing more to say\nNo more ace to play',
+                'The winner takes it all','The loser standing small','Beside the victory\nThat\'s her destiny',
+                'Money, money, money\nMust be funny','In the rich man\'s world','Money, money, money\nAlways sunny',
+                'In the rich man\'s world\nAha aha','All the things I could do','If I had a little money\nIt\'s a rich man\'s world',
+                'It's a rich man's world
+
+So when you're near me, darling can't you hear me
+S. O. S.
+The love you gave me, nothing else can save me
+S. O. S.
+When you're gone
+How can I even try to go on?
+When you're gone
+Though I try how can I carry on?
